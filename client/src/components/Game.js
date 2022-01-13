6617087eedba81d8d3380829442b7471d08abf30
binders <binderl@debian-BULLSEYE-live-builder-AMD64>
2022-01-13 13:23:59 +0100

    add front file

---
+++ client/src/components/Game.js
@@ -0,0 +1,39 @@
+import "../external/monopoly/styles.css"
+import MagicButton from "./MagicButton"
+import {useState} from "react";
+
+function Game(props) {
+  
+	const _networkId = props.networkId;
+	const _provider = props.provider;
+	const _address = props.address
+	const [render, setRender] = useState(false);
+	const rendering = () => {
+		if(render){
+			return(
+				<MagicButton/>
+			)
+		}
+	}
+	const blank = () => {
+		//var _blank = <br>;
+		var _blank = <div><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/></div>;
+		return(_blank)
+		
+
+	}
+
+	return (
+		<div className="vertical">
+			<input
+				type="button"
+				value="Magic Button"
+				onClick={()=>{setRender(true)}}
+			/>
+		{rendering()}
+		{blank()}
+		</div>
+	)
+}
+
+export default Game
---
+++ client/src/components/MagicButton.js
@@ -0,0 +1,18 @@
+
+
+
+function MagicButton (props) {
+	
+
+	return(
+		<img 
+			src="/images/MB.png" 
+			onClick={() => {
+				console.log("")
+			}}
+		/> 
+	)
+}
+
+
+export default MagicButton
