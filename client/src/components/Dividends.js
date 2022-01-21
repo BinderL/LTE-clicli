34d6bcf0c1cdd051987710da160136d607133f3a
binders <binderl@debian-BULLSEYE-live-builder-AMD64>
2022-01-21 13:05:04 +0100

    MW 44: restore front new face new skills

--- client/src/components/Dividends.js
+++ client/src/components/Dividends.js
@@ -1,6 +1,5 @@
 
 import React, {useState, useEffect} from 'react';
-import ObsBalance from "./ObsBalance";
 import {ethers} from "ethers"
 
 import Rogue from "../contracts/Rogue";
@@ -16,7 +15,8 @@ const Dividends = (props) => {
 	const [amount, setAmount] = useState("");
 	const [balance, setBalance] = useState(0);
 	const [deposit, setDeposit] = useState(0);
-	const [staker, setStaker] = useState(false);
+	const [staker, setStaker] = useState(null);
+
 
 	let MPs;
 	let stMPs;
@@ -40,10 +40,53 @@ const Dividends = (props) => {
 			contract();
     	setIsReadyToRender(true);
 	},[_provider, _networkId]);
+		
+	const Button = (_proto, _onClick) => {
+		return(
+		<input
+			onClick={_onClick}
+			type="button"
+			value={_proto}/>
+		);
+	}
+
+	const Input = (_proto, _onChange) => {
+		return(
+			<input type="text" value= {amount} id={_proto} onChange={_onChange}/>
+		);
+	}
+
+	const onChange = [
+		(e) => {setAmount(e.target.value);},
+	]
 
 
-	const render = () => {
-		if(staker)
+	const onClick = [
+		async() => {
+			var _balance = await MPs.balanceOf(_address);
+			setAmount(_balance.toNumber());
+			},
+		async()	=> {
+			await MPs.approve(stMPs.address, amount, {from:_address});
+			stMPs.depositAll();
+		}];
+
+	const _render = (_proto, _title) => {
+		return(
+			<div className="Staker">
+				<output className="title"> _title </output>
+				<div className="Buttons">
+					{Input(_proto, onChange[staker])}
+					{Button(_proto, onClick[staker])}
+					{Button(_proto, onClick[staker + 1])}
+				</div>
+			</div>
+		);
+	}
+
+	const _staker = () => {
+		if(staker){
+			
 			return(
 				<div>
 					<input 
@@ -70,12 +113,9 @@ const Dividends = (props) => {
 						value="Deposit"/>
 				</div>
 			);
+		}
 	}
-
-  if (!isReadyToRender) {
-    return (<div></div>)
-  }
-	else
+	const display = () => {
 		return(
 			<div className="Vertical">
 				<output className="title">EARNING</output>
@@ -88,12 +128,14 @@ const Dividends = (props) => {
 					<div className="Item-desc">
 						<output className="title">MPs tokens not staked </output> 
 						<output className="chiffre"> {balance + "  MPs tokens"} </output>
-						<input type="button" value="Stake"/>
+						<input type="button" value="Stake"
+							onClick={() => {setStaker(true);}}/>
 					</div>				
 					<div className="Item-desc">
 						<output className="title">Already staked </output> 
 						<output className="chiffre"> {deposit + "  MPs tokens"} </output>
 						<input type="button" value="Unstake"/>
+							onClick={() => {setStaker(false);}}/>
 					</div>
 				</div>
 				<output className="title"> Liquidity Mining </output>
@@ -107,15 +149,26 @@ const Dividends = (props) => {
 						<output className="title">MPs-WETH LP tokens balance </output> 
 						<output className="chiffre"> {balance + "  MPs-WETH LP tokens"} </output>
 						<input type="button" value="Stake"/>
+							onClick={() => {setStaker(false);}}/>
 					</div>				
 					<div className="Item-desc">
 						<output className="title">MPs-WETH LP tokens staked </output> 
 						<output className="chiffre"> {deposit + "  MPs-WETH LP tokens"} </output>
 						<input type="button" value="Unstake"/>
+							onClick={() => {setStaker(false);}}/>
 					</div>
 				</div>
 			</div>
 		);
+	}
+
+  if (!isReadyToRender) {
+    return (<div></div>)
+  }
+	else 
+		if(staker)
+			return _render("proto","title");
+		else return display();
 }
 
 export default Dividends;
